03e465d0a26e16c30405a6b8343fe1aaf4f3358c
MINALOTFY10 <menalotfy665@gmail.com>
2023-09-13 19:32:16 +0300

    commit Goal Tracker App

--- src/App.js
+++ src/App.js
@@ -1,46 +1,48 @@
-import React, {useState} from "react";
+import React, { useState } from 'react';
 
-import Expenses from "./components/Expenses/Expense";
-import NewExpense from "./components/NewExpense/NewExpense";
+import CourseGoalList from './components/CourseGoals/CourseGoalList/CourseGoalList';
+import CourseInput from './components/CourseGoals/CourseInput/CourseInput';
+import './App.css';
 
-const DUMMY_EXPENSES = [
-  {
-    id: 'e1',
-    title: 'Toilet Paper',
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-  },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-  {
-    id: 'e3',
-    title: 'Car Insurance',
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
-  },
-  {
-    id: 'e4',
-    title: 'New Desk (Wooden)',
-    amount: 450,
-    date: new Date(2021, 5, 12),
-  },
-];
+const App = () => {
+  const [courseGoals, setCourseGoals] = useState([
+    { text: 'Do all exercises!', id: 'g1' },
+    { text: 'Finish the course!', id: 'g2' }
+  ]);
 
-function App() {
-  
-  const [expenses, setExpenses] = useState(DUMMY_EXPENSES)
+  const addGoalHandler = enteredText => {
+    setCourseGoals(prevGoals => {
+      return [{ text: enteredText, id: Math.random().toString() }, ...prevGoals];
+    });
+  };
 
-  const addExpenseHandler = (expense) => {
-    setExpenses((prevExpenses) => {
-      return [expense, ...prevExpenses];
-    })
+  const deleteItemHandler = goalId => {
+    setCourseGoals(prevGoals => {
+      const updatedGoals = prevGoals.filter(goal => goal.id !== goalId);
+      return updatedGoals;
+    });
+  };
+
+  let content = (
+    <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
+  );
+
+  if (courseGoals.length > 0) {
+    content = (
+      <CourseGoalList items={courseGoals} onDeleteItem={deleteItemHandler} />
+    );
   }
 
   return (
     <div>
-       <NewExpense onAddExpense={addExpenseHandler}/>
-      <Expenses obj={expenses} />
+      <section id="goal-form">
+        <CourseInput onAddGoal={addGoalHandler} />
+      </section>
+      <section id="goals">
+        {content}
+      </section>
     </div>
   );
-}
+};
 
 export default App;
