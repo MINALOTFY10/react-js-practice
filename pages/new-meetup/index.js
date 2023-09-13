e75f23ba9b6bafd56d823c89863825009fda9d72
MINALOTFY10 <menalotfy665@gmail.com>
2023-09-13 21:42:13 +0300

    commit meetup using nextjs

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,37 @@
+import { Fragment } from "react";
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+import { useRouter } from "next/router";
+import Head from "next/head";
+
+export default function NewMeetupPage() {
+  const router = useRouter();
+
+  async function addMeetupHanlder(enteredMeetupData) {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    router.push("/");
+  }
+
+  return (
+    <Fragment>
+      <Head>
+        <title>Add a New Meetup</title>
+        <meta
+          name="description"
+          content="Add your own meetups and create amazing networking opportunities."
+        />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHanlder} />
+    </Fragment>
+  );
+}
